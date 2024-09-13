586e0863e9ce4d9e4abbd961be50e913c81dfc19
mofawzy <gisyah@yahoo.com>
2024-09-14 01:14:13 +0300

    lesson4

--- app.js
+++ app.js
@@ -1,11 +1,42 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3001;
+const mongoose = require('mongoose');
+app.use(express.urlencoded({ extended: true }));
+const Mydata = require("./models/mydataschema")
 
 app.get('/', (req, res) => {
   res.sendFile("./views/home.html",{root:__dirname})
 })
 
-app.listen(port, () => {
-  console.log(`http://localhost:${port}/`)
+app.get('/index.html', (req, res) => {
+  res.send("<h1>تم ارسال البيانات بنجاح</h1>")
+})
+
+
+mongoose
+.connect("mongodb+srv://mofawzy:hgvplkhgvpdl@cluster0.yjqwz.mongodb.net/all-data?retryWrites=true&w=majority&appName=Cluster0")
+.then( ()=>{
+    app.listen(port, () => {
+        console.log(`http://localhost:${port}/`)   
+        console.log(`https://github.com/mofawzy230/node-exp-level3`)
+    })
+})
+.catch((err)=>{console.log(err)});
+
+app.post('/', (req, res) => {
+  console.log(req.body)
+
+  const mydata = new Mydata(req.body);
+
+  mydata.save()
+  .then (()=>{
+    res.redirect("/index.html")
+  }) 
+  .catch((err)=>{
+    console.log(err)
+
+  })
+   
+  
 })
