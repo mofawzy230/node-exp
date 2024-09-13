dca4da9ca5f6fa41470010b9d1fa749adc351a4e
mofawzy <gisyah@yahoo.com>
2024-09-14 00:24:19 +0300

    project setup

--- app.js
+++ app.js
@@ -1,27 +1,11 @@
-const express = require('express');
-const app = express();
-const port = 3001;
-const mongoose = require('mongoose');
+const express = require('express')
+const app = express()
+const port = 3000
 
 app.get('/', (req, res) => {
   res.sendFile("./views/home.html",{root:__dirname})
 })
 
-app.get('/index.html', (req, res) => {
-  res.send("<h1>تم الاتصال بقاعدة البيانات بنجاح</h1>")
+app.listen(port, () => {
+  console.log(`http://localhost:${port}/`)
 })
-
-mongoose
-.connect("mongodb+srv://mofawzy:hgvplkhgvpdl@cluster0.iezt0.mongodb.net/all-data?retryWrites=true&w=majority&appName=Cluster0")
-.then( ()=>{
-    app.listen(port, () => {
-
-        console.log(`http://localhost:${port}/`)   
-        console.log(`https://github.com/mofawzy230/node-exp-level3`)
-    })
-    
-    res.redirect("/index.html")
-    
-})
-.catch((err)=>{console.log(err)});
-
