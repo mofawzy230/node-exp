28ccc4f25ed4ddf6b6a5d3364e46c151becbc1e3
mofawzy <gisyah@yahoo.com>
2024-09-24 08:22:49 +0300

    lesson13

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const User = require("./models/customerSchema");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 var moment = require('moment'); // require
-
+var methodOverride = require('method-override')
+app.use(methodOverride('_method'))
 
 //auto refresh
 const path = require("path");
@@ -43,15 +44,22 @@ app.get("/user/add.html", (req, res) => {
   res.render("user/add", {});
 });
 
-app.get("/user/edit.html", (req, res) => {
-  res.render("user/edit", {});
+app.get("/edit/:id", (req, res) => {
+  User.findById(req.params.id)
+     .then((result)=>{
+      res.render("user/edit",{item:result,moment: moment})
+     })
+     .catch((err) => {
+       console.log(err)
+     });
+  
 });
 
 app.get("/user/search.html", (req, res) => {
   res.render("user/search", {});
 });
 
-app.get("/user/:id", (req, res) => {
+app.get("/view/:id", (req, res) => {
   //rusult ==>  object
    User.findById(req.params.id)
      .then((result)=>{
@@ -76,6 +84,19 @@ app.post("/user/add.html", (req, res) => {
       console.log(err);
     });
 });
+
+// DELETE Request
+app.delete("/edit/:id",(req,res)=>{
+  // User.findByIdAndDelete(req.params.id) or
+  User.deleteOne({ _id: req.params.id })
+  .then(() => {
+    res.redirect("/");
+  })
+
+  .catch((err) => {
+    console.log(err);
+  });
+})
 
 mongoose
   .connect(
